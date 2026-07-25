e7663e7b119735442032c33ef7d6bf99514345d1
mosorio <mosorio@GV-TGU-MOSORIO.gvcorp.org>
2026-07-24 21:05:12 -0600

    clase 3

--- Hola_Mundo/src/hola_mundo/Hola_Mundo.java
+++ Hola_Mundo/src/hola_mundo/Hola_Mundo.java
@@ -4,6 +4,8 @@
  */
 package hola_mundo;
 
+import java.util.Set;
+
 /**
  *
  * @author mosorio
@@ -15,6 +17,44 @@ public class Hola_Mundo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        
+        System.out.println("hola me llamo Manuel");
+        System.out.println("mi primer programa");
+        
+        System.out.print("vamos camino a");
+        System.out.print("la clase de");
+        System.out.print("progra");
+        System.out.println("");
+        
+        System.out.printf("Tu apellido es %S \n","osorio");
+        //System.out.println("");
+        
+        System.out.printf("%S Tu apellido es","osorio");
+        System.out.println("");
+        
+        System.out.printf("Tu apellido %S es \n","osorio");
+        System.out.println("Hola \n este es \n un nuevo");
+        System.out.println("hola \t este es \t un nuevo");
+        
+        //variables
+        int edad; //tipo entero
+        double peso; //tipo Decimal
+        char inicial; //tipo caracter letras digitos simbolos especiales  
+        String nombre; //tipo cadena
+        boolean estado; //tipo estado true / false
+        
+        edad = 21;
+        peso = 60.5;
+        inicial = 'D';
+        nombre = "Manuel";
+        
+        System.out.printf("Tengo %d anios \n" ,edad);
+        System.out.printf("Mi peso ideal es %.2f \n" ,peso);
+        System.out.printf("Dime %c por mi inicial \n" ,inicial);
+        System.out.printf("%s es mi nombre \n" ,nombre);
+        
+        System.out.printf("%s",nombre);
+                
+    }//fin de main
     
-}
+    }//fin de class
